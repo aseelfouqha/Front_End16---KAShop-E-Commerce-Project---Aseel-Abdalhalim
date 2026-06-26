9180eec1224f6e227b19657aac17c4dbc3b045a1
aseelfouqha <aseel.abdulhalim@gmail.com>
2026-06-26 18:13:56 +0300

    register code is done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Button variant="outlined">Outlined</Button>
+    </div>
   )
 }
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { Box } from "@mui/material/Box"
+import Typography from '@mui/material/Typography'
+import { TextField } from '@mui/material/TextField';
+import { useForm } from 'react-hook-form';
+import Button from '@mui/material/Button';
+import axios from 'axios'
+
 
 export default function Register() {
+  const {register, handleSubmit} = useForm();
+  const RegisterForm = async(data) => {
+    try{
+      const response = await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/Register`,data);
+      console.log(response);
+    }catch(err){
+      console.log(err);
+    }
+  }
   return (
-    <div>Register</div>
+    <Box component="section" className="registerPage"> 
+        <Typography component="h1" variant="h2">
+          Register
+        </Typography>
+        <Box onSubmit={handleSubmit(RegisterForm)} component="form" sx={{marginTop:2, display:'flex', flexDriection:'Column', gap:2}}>
+          <TextField fullWidth {...register("userName")} label="userName" variant="outlined"/>
+          <TextField fullWidth {...register("fullName")} label="fullName" variant="outlined"/>
+          <TextField fullWidth {...register("email")} label="email" variant="outlined"/>
+          <TextField fullWidth {...register("phoneNumber")} label="phoneNumber" variant="outlined"/>
+          <TextField fullWidth {...register("password")} label="password" variant="outlined"/>
+          <Button variant="contained" type="sumbit">
+            Register
+          </Button>
+        </Box>
+    </Box>
   )
 }
