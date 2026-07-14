e8fe655b264255473c4f62d94fe4c7448a912cc0
aseelfouqha <aseel.abdulhalim@gmail.com>
2026-07-14 19:46:06 +0300

    fix the LoginSchema

--- src/Validations/LoginSchema.jsx
+++ src/Validations/LoginSchema.jsx
@@ -3,9 +3,6 @@ import * as yup from 'yup';
 
 
   export  const loginSchema = yup.object ({
-    userName:yup.string().required().min(3).max(20),
-    fullName:yup.string().required().min(3).max(20),
     email:yup.string().email().required(),
-    phoneNumber:yup.string().required(),
     password:yup.string().required(),
   })
